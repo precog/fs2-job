84bb5930b048fceee3693ff9d63235fbe5237c46
Daniel Spiewak <daniel@slamdata.com>
2019-05-08 11:54:34 -0600

    Starting point on signature noodling

---
+++ core/src/main/scala/fs2/job/JobManager.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014–2018 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fs2
+package job
+
+import cats.effect.Concurrent
+
+import scala.{Predef, Unit}, Predef.???
+
+final class JobManager[F[_], I, N, R] private () {
+
+  def notifications: Stream[F, N] = ???
+
+  def submit(job: Job[F, I, N, R]): F[Unit] = ???
+
+  def tap(job: Job[F, I, N, R]): Stream[F, R] = ???
+
+  def cancel(id: I): F[Unit] = ???
+
+  def status(id: I): F[Status] = ???
+
+  private def run: Stream[F, Unit] = ???
+  private def shutdown: F[Unit] = ???
+}
+
+object JobManager {
+
+  def apply[F[_]: Concurrent, I, N, R]: Stream[F, JobManager[F, I, N, R]] = {
+    val initF = Concurrent[F].delay(new JobManager[F, I, N, R])
+    Stream.bracket(initF)(_.shutdown) flatMap { jm =>
+      Stream.emit(jm).concurrently(jm.run)
+    }
+  }
+}
